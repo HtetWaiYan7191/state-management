f12788466896d00427774c5dccc5cf78ac912530
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-11-13 23:10:55 +0630

    Remove reverse method

--- app/team/page.jsx
+++ app/team/page.jsx
@@ -9,7 +9,6 @@ import { fetchPlayers } from "@/redux/features/playerSlice";
 const Team = () => {
   const players = useSelector((state) => state.playerReducer.value);
   const teams = useSelector((state) => state.playerReducer.team);
-  const reverseTeams = [...teams].reverse();
   const dispatch = useDispatch();
   
   useEffect(() => {
@@ -24,8 +23,8 @@ const Team = () => {
       <Navigation />
       <AddTeam players={players} />
       <main className="team-container mt-10 w-[80%] mx-auto grid grid-cols-1 gap-8 md:grid-cols-2 md:gap-10 lg:grid-cols-4">
-        {reverseTeams && reverseTeams.length > 0 ? (
-          reverseTeams.map((team) => <TeamCard team={team} key={team.id} />)
+        {teams && teams.length > 0 ? (
+          teams.map((team) => <TeamCard team={team} key={team.id} />)
         ) : (
           <span className="px-4 block">No Teams At This Moment !!!</span>
         )}
