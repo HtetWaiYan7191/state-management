0a3212b8ecdf33c7ac1e609483b304f8ae7f03fb
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-11-13 12:34:24 +0630

    Implement logout functionality

--- app/components/Login.jsx
+++ app/components/Login.jsx
@@ -3,7 +3,7 @@ import { logIn } from '@/redux/features/authSlice';
 import React, { useState } from 'react'
 import { useDispatch } from 'react-redux';
 import { useRouter } from 'next/navigation';
-
+import Button from './Button';
 const Login = () => {
     const [name, setName] = useState('');
     const router = useRouter();
@@ -23,13 +23,8 @@ const Login = () => {
     <div className='login-container flex justify-center items-center h-[100vh]'>
         <form action="" onSubmit={handleSubmit} className='border border-2 p-5 rounded-md'>
             <h2 className='text-center my-2 font-semibold text-lg'>LOGIN</h2>
-            <input type="text" onChange={handleChange} value={name} placeholder="Enter Name" className="input input-sm lg:input-md input-bordered  w-full max-w-xs mt-4" />
-
-            <button type='submit' className=" px-4 py-2 bg-slate-800 text-white font-semibold rounded-md mt-4 hover:bg-slate-500">
-                LogIn
-            </button>   
-
-
+            <input type="text" onChange={handleChange} value={name} placeholder="Enter Name" className="input input-sm lg:input-md input-bordered  w-full max-w-xs my-2" />
+                <Button title="LogIn"/>  
         </form>
     </div>
   )
--- app/main/page.jsx
+++ app/main/page.jsx
@@ -1,12 +1,27 @@
 'use client'
 import React from 'react'
-import {useSelector} from 'react-redux' 
+import {useDispatch, useSelector} from 'react-redux' 
+import Button from '../components/Button';
+import { useRouter } from 'next/navigation';
+import { logOut } from '@/redux/features/authSlice';
 const Main = () => {
     const currentUser =  useSelector((state) => state.authReducer.value.username);
+    const dispatch = useDispatch();
+    const router = useRouter();
+
+    const handleOnClick = (e) => {
+      e.preventDefault();
+      dispatch(logOut());
+      router.push('/')
+    }
   return (
-    <div>
-       <h2 className='text-white'>{currentUser}</h2>
-    </div>
+    <section className='main-page flex justify-center'>
+      <header className='flex gap-5 items-center mt-10'>
+          <h2 className='text-white capitalize font-bold text-2xl'>{currentUser}</h2>
+          <Button title={"LogOut"} handleClick={handleOnClick}/>
+      </header>
+       
+    </section>
   )
 }
 
